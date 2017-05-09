b4b38a60524d0babba4ef09e2848fa52bacea9f8
em <eugen.mayer@kontextwork.de>
2017-05-09 23:36:20 +0200

    use unison for the initial synchronious sync so we respect ignores

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM alpine:edge
 
 ARG UNISON_VERSION=2.48.4
-RUN apk add --no-cache build-base curl bash supervisor inotify-tools rsync\
+RUN apk add --no-cache build-base curl bash supervisor inotify-tools rsync ruby\
     && apk add --update-cache --repository http://dl-4.alpinelinux.org/alpine/edge/testing/ ocaml \
     && curl -L https://github.com/bcpierce00/unison/archive/$UNISON_VERSION.tar.gz | tar zxv -C /tmp \
     && cd /tmp/unison-${UNISON_VERSION} \
--- precopy_appsync.sh
+++ precopy_appsync.sh
@@ -5,12 +5,14 @@ HOST_VOLUME=${HOST_VOLUME:-/host_sync}
 OWNER_UID=${OWNER_UID:-0}
 
 if [ ! -f /unison/initial_sync_finished ]; then
-	echo "doing initial sync with cp"
-	time cp -au  $HOST_VOLUME/.  $APP_VOLUME
+	echo "doing initial sync with unison"
+	# we use ruby due to http://mywiki.wooledge.org/BashFAQ/050
+	time ruby -e '`unison #{ENV["UNISON_ARGS"]} #{ENV["UNISON_PREFER"]} #{ENV["UNISON_EXCLUDES"]} -numericids -auto -batch /host_sync /app_sync`'
+	#time cp -au  $HOST_VOLUME/.  $APP_VOLUME
 	echo "chown ing file to uid $OWNER_UID"
 	chown -R $OWNER_UID $APP_VOLUME
 	touch /unison/initial_sync_finished
-	echo "initial sync done using cp" >> /tmp/unison.log
+	echo "initial sync done using unison" >> /tmp/unison.log
 else
-	echo "skipping initial cp"
+	echo "skipping initial copy with unison"
 fi
